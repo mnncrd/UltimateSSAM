495d5c1a7946607dafcbaf1fc5775ac899814ed6
mnncrd <manon.curaudeau@cri-paris.org>
2019-12-08 18:35:40 +0100

    Add function to open .pdb file

--- src/ssam.py
+++ src/ssam.py
@@ -9,6 +9,19 @@ This program assigns secondary structures to a sequence.
 
 import argparse
 
+def open_pdb_file(filename):
+
+    """Reads the .pdb file and stores info"""
+
+    assert filename.lower().endswith(".pdb"), "Program can only work with a .pdb file"
+    print("Reading file")
+    file_pdb = open(filename, "r")
+    file_pdb.close()
+    pdb_info = []
+    residues = []
+    print("ok")
+    return pdb_info, residues
+
 def main():
 
     """Main function"""
@@ -19,3 +32,10 @@ def main():
     parser.add_argument("-o", type=str)
     parser.add_argument("-hy", "--hydrogen", action="store_true", help="add hydrogen atoms")
     args = parser.parse_args()
+
+    #Read the file
+    try:
+        pdb_info, residues = open_pdb_file(args.filename)
+    except AssertionError as error:
+        print(error)
+        print(args.filename, " is not a .pdb file")
