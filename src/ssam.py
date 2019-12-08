796efa038800c133c1c6e2b3310ee577c9a8b7f6
mnncrd <manon.curaudeau@cri-paris.org>
2019-12-09 00:37:39 +0100

    Add function to read .pdb file

--- src/ssam.py
+++ src/ssam.py
@@ -9,17 +9,38 @@ This program assigns secondary structures to a sequence.
 
 import argparse
 
+def read_pdb_file(lines):
+
+    """Stores info"""
+
+    header_pdb = lines[0].strip()
+    authors = []
+    organism = ""
+    molecule = ""
+    residues = []
+    res_nb = 0
+    atoms = []
+    for line in lines:
+        if 'ORGANISM_SCIENTIFIC: ' in line:
+            organism += line.strip()
+        elif 'MOLECULE: ' in line:
+            molecule += line.strip()
+        elif line[0:6] == 'AUTHOR':
+            authors.append(line[10:].strip().split(","))
+    authors = ','.join(sum(authors, []))
+    pdb_info = (header_pdb, organism, molecule, authors)
+    return pdb_info, residues
+
 def open_pdb_file(filename):
 
-    """Reads the .pdb file and stores info"""
+    """Opens the .pdb file and read the lines"""
 
     assert filename.lower().endswith(".pdb"), "Program can only work with a .pdb file"
     try:
         with open(filename, "r") as file_pdb:
-            list_of_lines = file_pdb.readlines()
+            lines = file_pdb.readlines()
+            pdb_info, residues = read_pdb_file(lines)
         file_pdb.close()
-        pdb_info = []
-        residues = []
         print("ok")
         return pdb_info, residues
     except FileNotFoundError as fnf_error:
