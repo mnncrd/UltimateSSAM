298f84da7d9ad7d964e6fb76f966778c885b124d
mnncrd <manon.curaudeau@cri-paris.org>
2019-12-10 18:42:46 +0100

    Add function to compute the difference between two vectors

--- src/vectors.py
+++ src/vectors.py
@@ -19,3 +19,10 @@ def scal_product(v_i, v_j):
 
     scal_prod = v_i[0]*v_j[0] + v_i[1]*v_j[1] + v_i[2]*v_j[2]
     return scal_prod
+
+def compute_diff_vect(v_i, v_j):
+
+    """Computes the diffence between two vectors"""
+
+    v_ij = [i-j for i, j in zip(v_i, v_j)]
+    return v_ij
