f96df5db1568373bce355d2624a5ad313dac81c7
mnncrd <manon.curaudeau@cri-paris.org>
2019-12-10 10:31:53 +0100

    Add function to compute angles between two vectors

--- src/angles.py
+++ src/angles.py
@@ -2,3 +2,16 @@
 
 This module computes angles between vectors.
 """
+
+import math
+import vectors
+
+def compute_angle(v_i, v_j):
+
+    """Computes the angle between two vectors"""
+
+    scal_prod = vectors.scal_product(v_i, v_j)
+    v_i_norm = vectors.vector_norm(v_i)
+    v_j_norm = vectors.vector_norm(v_j)
+    angle = math.acos(scal_prod/(v_i_norm*v_j_norm))
+    return angle
