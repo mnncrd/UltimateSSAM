4e970b91849bf9f75126a07fa89fafcb3e23a192
mnncrd <manon.curaudeau@cri-paris.org>
2020-01-02 17:24:10 +0100

    Add ssbridges module

---
+++ src/ssbridges.py
@@ -0,0 +1,16 @@
+"""SS-bridges module.
+
+This module deals with SS-bridges. For instance, it assign ss-bridges.
+"""
+
+def assign_ss_bond(residues, ss_bonds):
+
+    """Assign SS-bonds"""
+
+    alphabet = {i:chr(65+i).lower() for i in range(26)}
+    for res in residues:
+        for i, bond in enumerate(ss_bonds):
+            if res.chain == bond[0][0] and res.number == bond[0][1]:
+                res.name = alphabet[i%26]
+            elif res.chain == bond[1][0] and res.number == bond[1][1]:
+                res.name = alphabet[i%26]
