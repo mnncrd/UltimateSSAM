5a42f51c35abdfd17d39ab37f4fed28408074ba0
mnncrd <manon.curaudeau@cri-paris.org>
2020-01-02 17:33:59 +0100

    Add count of ss_bonds

--- src/ssbridges.py
+++ src/ssbridges.py
@@ -3,6 +3,18 @@
 This module deals with SS-bridges. For instance, it assign ss-bridges.
 """
 
+def count_ss_bonds(ss_bonds):
+
+    intra = 0
+    inter = 0
+    for bond in ss_bonds:
+        if bond[0][0] == bond[1][0]:
+            intra += 1
+        else:
+            inter += 1
+    nb_ss = (intra, inter)
+    return(nb_ss)
+
 def assign_ss_bonds(residues, ss_bonds):
 
     """Assign SS-bonds"""
